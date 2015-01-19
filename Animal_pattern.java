c1e3dfc02c63418740662205b49a59be821f84a0
xawroz <saroj_0342@deerwalk.edu.np>
2015-01-19 13:39:23 +0545

    java deer
    deer made by printing symbols in java

---
+++ Animal_pattern.java
@@ -0,0 +1,16 @@
+/**
+ * Created by saroj on 1/19/2015.
+ */
+public class Animal_pattern {
+    public static void main(String[] args) {
+        System.out.println ("           '____'");
+        System.out.println ("           ( \u00A9\u00A9 )" );
+        System.out.println ("  /==========\\/");
+        System.out.println (" /  ||  %%  ||");
+        System.out.println ("*   ||------||");
+        System.out.println ("    \u00A5\u00A5      \u00A5\u00A5");
+        System.out.println ("    \"\"      \"\"");
+        System.out.println ("");
+
+    }
+}
